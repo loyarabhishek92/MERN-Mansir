c173761840c9109b40f9eb1836db681730071feb
loyarabhishek92 <loyarabhishek92@gmail.com>
2026-01-06 17:04:37 +0545

    changes

--- index.js
+++ index.js
@@ -27,4 +27,54 @@
 // console.log(`he is good ${personName} ${age} ${job}`);
 // console.log(`he is good ${personName} ${age} ${job}`);
 // console.log("abishek\"".length);
-console.log("abishek\"".toUpperCase());
+/*console.log("abishek\"".toUpperCase());
+console.log("abishek\"".toLowerCase());*/
+
+// let str = "plese give Rs 1000";
+// console.log(str.match)
+/*const text = "plese give me rs 1000";
+
+const match = text.match(/\d+/);
+const amount = match ? Number(match[0]) : null;
+
+console.log(amount); // 1000*/
+
+// console.log("Bibek");
+
+
+
+/*const temperature = 20;
+const value = 2;
+const pizza = 'pizza';
+const pasta = 'pasta';
+
+console.log(`The temperature is ${temperature}C\n ${value} plus ${value} makes ${value+value}\n I like ${pizza} and ${pasta}.`);*/
+
+
+let a='9000';
+// console.log(Number(a) + 50);
+console.log(Number.isNaN(Number(a)));
+
+
+const car = {
+    Name: "BMW",
+    color: "black",
+    model: 154215,
+    speed: function() {
+        console.log("the speed is 250km/h");
+    }
+}
+
+const bank ={
+    Name: 'Nepal Bank ltd',
+    location: 'Thapathali',
+    grade:'A',
+    funding:function() {
+        console.log("government funding")
+    }
+
+}
+// function calling inside object
+bank.funding();
+// value calling inside object 
+console.log(bank.grade);
